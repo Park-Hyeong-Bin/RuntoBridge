4446a6d85233f6ca1e0c6c310081e7e725daf1ae
박형빈 <cavin1999@gmail.com>
2026-05-13 11:41:14 +0900

    feat : 필요에셋 추가 + 플레이어 컨트롤 + FloorActor 생성

---
+++ Source/Run_To_Bridge/Private/Floor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Floor.h"
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AFloor::AFloor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 박스 콜리전 컴포넌트 생성
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+	
+	// 생성한 박스 콜리전 컴포넌트를 최상단 컴포넌트로 설정
+	SetRootComponent(boxComp);
+	
+	// 스태틱메시 컴포넌트 생성
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My Mesh Component"));
+	
+	// 박스 콜리전 자식으로 스태틱 메시 컴포넌트 설정
+	meshComp->SetupAttachment(boxComp);
+	
+	// 박스 콜라이더 크기를 300x300x50 설정 (Extent는 절반인 150x150x25)
+	FVector boxSize = FVector(150.f, 150.f, 25.f);
+	boxComp->SetBoxExtent(boxSize);
+
+	// 메쉬 컴포넌트 크기를 박스 콜라이더와 맞춤 (기본 큐브 100x100x100 기준)
+	meshComp->SetRelativeScale3D(FVector(3.f, 3.f, 0.5f));
+	}
+
+// Called when the game starts or when spawned
+void AFloor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Run_To_Bridge/Public/Floor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Floor.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class RUN_TO_BRIDGE_API AFloor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFloor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// 박스 충돌체 컴포넌트
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* boxComp;
+	
+	// 스태틱 메시 컴포넌트
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* meshComp;
+};
